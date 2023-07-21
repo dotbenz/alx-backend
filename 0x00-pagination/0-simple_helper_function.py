61aa99910f3deb292060bc0937591473973f375d
Ebenezer Adedotun <dotbenzer@yahoo.com>
2023-07-20 20:34:42 -0700

    Solution to pagination task

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,19 +1,24 @@
-def index_range(page: int, page_size: int) -> tuple:
+#!/usr/bin/env python3
+"""
+Contains definition of index_range helper function
+"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
-    Calculate the start and end index for a given page and page size in a paginated list.
-
-    Parameters:
-        page (int): The page number (1-indexed).
-        page_size (int): The number of items per page.
-
-    Returns:
-        tuple: A tuple containing the start index and end index for the specified page.
+    Takes 2 integer arguments and returns a tuple of size two
+    containing the start and end index corresponding to the range of
+    indexes to return in a list for those pagination parameters
+    Args:
+        page (int): page number to return (pages are 1-indexed)
+        page_size (int): number of items per page
+    Return:
+        tuple(start_index, end_index)
     """
-    if page <= 0 or page_size <= 0:
-        raise ValueError("Both 'page' and 'page_size' should be positive integers.")
+    start, end = 0, 0
+    for i in range(page):
+        start = end
+        end += page_size
 
-    start_index = (page - 1) * page_size
-    end_index = start_index + page_size - 1
-
-    return start_index, end_index
-
+    return (start, end)
